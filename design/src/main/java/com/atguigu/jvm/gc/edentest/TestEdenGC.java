9700b51550740674964c68c2c6d4884e36711774
yangjiang <1572467891@qq.com>
2020-10-22 20:04:45 +0800

    生产者消费者虚假唤醒

---
+++ design/src/main/java/com/atguigu/jvm/gc/edentest/TestEdenGC.java
@@ -0,0 +1,19 @@
+package com.atguigu.jvm.gc.edentest;
+
+/**
+ * @description: eden space的垃圾回收测试
+ * @author: yangjiang
+ * @create: 2020-10-22 17:54
+ **/
+public class TestEdenGC {
+
+    private static final int _1MB=1024*1024;
+    public static void main(String[] args) {
+        byte[] a1,a2,a3,a4;
+        a1=new byte[2*_1MB];
+        a2=new byte[2*_1MB];
+        a3=new byte[2*_1MB];
+        a4=new byte[4*_1MB];
+
+    }
+}
